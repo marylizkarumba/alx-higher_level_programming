7e6b5dafcb71845765548d0d28cbe04bcaa0311d
marylizkarumba <marylizkarumba791@gmail.com>
2022-10-21 07:57:17 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+"""This module contains a script that finds a peak in a list of unsorted
+integers"""
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m]
+    """Finds a peak in a list of unsorted integers"""
+    length = len(list_of_integers)
+    if length == 0:
+        return None
+    peak = re_peak(list_of_integers, 0, length - 1)
+    return peak
+
+
+def re_peak(int_list, start, end):
+    if start == end:
+        return int_list[start]
+    mid = int((start + end) // 2)
+    if int_list[mid] > int_list[mid + 1] and int_list[mid] > int_list[mid - 1]:
+        return int_list[mid]
+    if int_list[mid + 1] > int_list[mid]:
+        return re_peak(int_list, mid + 1, end)
+    else:
+        return re_peak(int_list, 0, mid - 1)
